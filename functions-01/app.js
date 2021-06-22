f35d2cf035604a9b2d79c5089d6eb9d2662827d6
BerkayUslu <mberkayuslu@outlook.com>
2021-06-22 21:43:52 +0300

    dayEnd

--- functions-01/app.js
+++ functions-01/app.js
@@ -1,1 +1,16 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
+
+
+function gameStarter(){
+    console.log("GAME STARTING...");
+    const playerMove = prompt("Please enter your chose rock/paper/scissors");
+    if(playerMove.trim() == "" || ){
+
+    }
+}
+
+startGameBtn.addEventListener('click', gameStarter);
