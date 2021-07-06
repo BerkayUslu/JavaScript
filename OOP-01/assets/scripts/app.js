0c8289c7bfa313d75646dccc3fda5a346e0a07fc
BerkayUslu <mberkayuslu@outlook.com>
2021-07-06 07:16:05 +0300

    up

--- OOP-01/assets/scripts/app.js
+++ OOP-01/assets/scripts/app.js
@@ -1,96 +1,93 @@
-const actvId = document.getElementById('active-projects').querySelector('ul');
-const fnshId = document.getElementById('finished-projects').querySelector('ul');
-
-const courseCardId = actvId.querySelector('#p1');
-const grocCardId = actvId.querySelector('#p2');
-const hotelCardId = fnshId.querySelector('#p3');
-
-const courseInfoBtn = courseCardId.querySelector('button');
-const courseToggleBtn = courseInfoBtn.nextElementSibling;
-const grocInfoBtn = grocCardId.querySelector('button');
-const grocToggleBtn = grocInfoBtn.nextElementSibling;
-const hotelInfoBtn = hotelCardId.querySelector('button');
-const hotelToggleBtn = hotelInfoBtn.nextElementSibling;
-
-class Project {
-  constructor(title, subText, moreInfo, status) {
-    this.title = title;
-    this.subText = subText;
-    this.moreInfo = moreInfo;
-    this.status = status;
+class DOMhandler {
+  static appendToDom(newAddress, projectItem) {
+    newAddress.append(projectItem);
+  }
+  static updateDOM(projectItem, type, fnc) {
+    const switchBtn = projectItem.querySelectorAll('button')[1];
+    switchBtn.textContent = type;
+    switchBtn.addEventListener('click', fnc.bind(null, projectItem.id));
+  }
+  static clearEventListeners(element) {
+    const clonedElement = element.cloneNode(true);
+    element.replaceWith(clonedElement);
+    return clonedElement;
   }
 }
 
-const projectList = {
-  data: [],
-  addData: () => {
-    this.data = [
-      Project(
-        'Finish the Course',
-        'Finish the course within the next two weeks.',
-        'Got lifetime access, but would be nice to finish it soon!',
-        0
-      ),
-      Project(
-        'Buy Groceries',
-        "Don't forget to pick up groceries today.",
-        'Not really a business topic but still important.',
-        0
-      ),
-      Project(
-        'Book Hotel',
-        "Academind conference takes place in December, don't forget to book a hotel.",
-        'Super important conference! Fictional but still!',
-        1
-      ),
-    ];
-  },
-};
+class Info {}
 
-const finActButtons = {
-  toggleBtwActFin: (projectDom, buttonDom) => {
-    if (buttonDom.textContent === 'Activate') {
-      buttonDom.textContent = 'Finish';
-      actvId.append(projectDom);
-    } else {
-      buttonDom.textContent = 'Activate';
-      fnshId.append(projectDom);
+class Project {
+  constructor(id, switchFunction) {
+    this.id = id;
+    this.switchHandler = switchFunction;
+    this.projectDom = document.getElementById(id);
+    this.infoButtonDom = this.projectDom.querySelector('button');
+
+    this.switchButtonDom = this.infoButtonDom.nextElementSibling;
+    this.switchButtonActivate();
+    this.moreInfoButtonActivate();
+  }
+  switchButtonActivate() {
+    //burada butona evebt listener ekleyecek ve proje idsini bind edeceğim
+    this.switchButtonDom.addEventListener(
+      'click',
+      this.switchHandler.bind(null, this.id)
+    ); //buraya bulunduğu listeye gidip elementi kaldıracak bir fonksiyon gerek
+  }
+  moreInfoButtonActivate() {}
+}
+
+class ProjectList {
+  projects = [];
+  constructor(statusName, btnText) {
+    this.buttonText = btnText;
+    this.statusName = statusName;
+    const listElements = document.querySelectorAll(
+      `#${statusName}-projects li`
+    );
+    for (const element of listElements) {
+      this.projects.push(
+        new Project(element.id, this.switchHandler.bind(this))
+      );
     }
-  },
-  finActBtnEventLi: () => {
-    courseToggleBtn.addEventListener(
-      'click',
-      finActButtons.toggleBtwActFin.bind(null, courseCardId, courseToggleBtn)
-    );
-    grocToggleBtn.addEventListener(
-      'click',
-      finActButtons.toggleBtwActFin.bind(null, grocCardId, grocToggleBtn)
-    );
-    hotelToggleBtn.addEventListener(
-      'click',
-      finActButtons.toggleBtwActFin.bind(null, hotelCardId, hotelToggleBtn)
-    );
-  },
-};
-const moreInfoButtons = {
-  moreInf: (projectDom) => {
-    alert(projectDom.dataset.extraInfo);
-  },
-  moreInfEvent: () => {
-    courseInfoBtn.addEventListener(
-      'click',
-      moreInfoButtons.moreInf.bind(null, courseCardId)
-    );
-    grocInfoBtn.addEventListener(
-      'click',
-      moreInfoButtons.moreInf.bind(null, grocCardId)
-    );
-    hotelInfoBtn.addEventListener(
-      'click',
-      moreInfoButtons.moreInf.bind(null, hotelCardId)
-    );
-  },
-};
+  }
 
-finActButtons.finActBtnEventLi();
-moreInfoButtons.moreInfEvent();
+  switchHandler(id) {
+    const index = this.projects.findIndex((obj) => obj.id === id);
+    console.log(this.projects);
+    const projectItemElement = document.getElementById(id);
+    let switchBtn = projectItemElement.querySelector('button:last-of-type');
+    switchBtn = DOMhandler.clearEventListeners(switchBtn);
+    this.switchAddHandler(this.projects[index]);
+    this.projects = this.projects.filter((obj)=>obj.id != id);
+  }
+  switchAddBridge(switchAdd) {
+    this.switchAddHandler = switchAdd;
+  }
+  switchAdd(projectClassInstance) {
+    this.projects.push(projectClassInstance);
+    const projectItem = document.getElementById(projectClassInstance.id);
+    const listDomId = document.querySelector(`#${this.statusName}-projects ul`);
+    DOMhandler.appendToDom(listDomId, projectItem);
+    DOMhandler.updateDOM(
+      projectItem,
+      this.buttonText,
+      this.switchHandler.bind(this)
+    );
+  }
+}
+
+class App {
+  static init() {
+    const activeProjectsList = new ProjectList('active', 'Finish');
+    const finishedProjectsList = new ProjectList('finished', 'Activate');
+    activeProjectsList.switchAddBridge(
+      finishedProjectsList.switchAdd.bind(finishedProjectsList)
+    );
+    finishedProjectsList.switchAddBridge(
+      activeProjectsList.switchAdd.bind(activeProjectsList)
+    );
+  }
+}
+
+App.init();
